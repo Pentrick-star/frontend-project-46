156edb1d525d3b1e3785f29705c9caef613333a1
Pentrick <bibenina09@mail.ru>
2025-07-07 19:47:58 +0300

    добавлены изменения

--- gendiff.js
+++ gendiff.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import genDiff from './src/genDiff.js';
+import genDiff from '../src/genDiff.js';
 
 const program = new Command();
 
 program
+  .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
@@ -16,8 +17,10 @@ program
     console.log(diff);
   });
 
-program.parse(process.argv);
-
+// --- ЭТО ВАЖНО ---
 if (process.argv.length <= 2) {
   program.outputHelp();
+  // НЕ вызывай process.exit()
+} else {
+  program.parse(process.argv);
 }
