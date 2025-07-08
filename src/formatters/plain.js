5424654d08d40d28646f25114553716704289ef6
Pentrick <bibenina09@mail.ru>
2025-07-08 15:19:25 +0300

    исправление отступов и кавычек

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -11,16 +11,16 @@ const iter = (tree, ancestry = []) => tree
     const property = [...ancestry, node.key].join('.')
 
     switch (node.type) {
-    case 'added':
-      return `Property '${property}' was added with value: ${stringify(node.value)}`
-    case 'removed':
-      return `Property '${property}' was removed`
-    case 'changed':
-      return `Property '${property}' was updated. From ${stringify(node.oldValue)} to ${stringify(node.newValue)}`
-    case 'nested':
-      return iter(node.children, [...ancestry, node.key])
-    default:
-      return []
+      case 'added':
+        return `Property '${property}' was added with value: ${stringify(node.value)}`
+      case 'removed':
+        return `Property '${property}' was removed`
+      case 'changed':
+        return `Property '${property}' was updated. From ${stringify(node.oldValue)} to ${stringify(node.newValue)}`
+      case 'nested':
+        return iter(node.children, [...ancestry, node.key])
+      default:
+        return []
     }
   })
 
