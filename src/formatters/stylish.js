834424f2b73d64b5b9f49f50db24bc16691ec6e0
Pentrick <bibenina09@mail.ru>
2025-07-08 23:26:14 +0300

    поправлены отсупы

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,38 +1,43 @@
-const getIndent = (depth, sign = ' ') => '    '.repeat(depth - 1) + sign + ' '
+const getIndent = (depth) => '    '.repeat(depth);
+
+const buildLine = (key, value, depth, sign = ' ') => {
+  const indent = getIndent(depth);
+  return `${indent}  ${sign} ${key}: ${value}`;
+};
 
 const stringify = (value, depth) => {
   if (typeof value !== 'object' || value === null) {
-    return String(value)
+    return String(value);
   }
 
-  const entries = Object.entries(value)
+  const entries = Object.entries(value);
   const lines = entries.map(
     ([key, val]) => `${getIndent(depth + 1)}${key}: ${stringify(val, depth + 1)}`,
-  )
-  return `{\n${lines.join('\n')}\n${'    '.repeat(depth)}}`
-}
+  );
+  return `{\n${lines.join('\n')}\n${getIndent(depth)}}`;
+};
 
-const stylish = (ast, depth = 1) => {
+const stylish = (ast, depth = 0) => {
   const lines = ast.map((node) => {
     switch (node.type) {
-    case 'added':
-      return `${getIndent(depth, '+')}${node.key}: ${stringify(node.value, depth)}`
-    case 'removed':
-      return `${getIndent(depth, '-')}${node.key}: ${stringify(node.value, depth)}`
-    case 'unchanged':
-      return `${getIndent(depth)}${node.key}: ${stringify(node.value, depth)}`
-    case 'changed': {
-      const oldLine = `${getIndent(depth, '-')}${node.key}: ${stringify(node.oldValue, depth)}`
-      const newLine = `${getIndent(depth, '+')}${node.key}: ${stringify(node.newValue, depth)}`
-      return `${oldLine}\n${newLine}`
+      case 'added':
+        return buildLine(node.key, stringify(node.value, depth + 1), depth, '+');
+      case 'removed':
+        return buildLine(node.key, stringify(node.value, depth + 1), depth, '-');
+      case 'unchanged':
+        return buildLine(node.key, stringify(node.value, depth + 1), depth);
+      case 'changed': {
+        const oldLine = buildLine(node.key, stringify(node.oldValue, depth + 1), depth, '-');
+        const newLine = buildLine(node.key, stringify(node.newValue, depth + 1), depth, '+');
+        return `${oldLine}\n${newLine}`;
+      }
+      case 'nested':
+        return `${getIndent(depth)}  ${node.key}: ${stylish(node.children, depth + 1)}`;
+      default:
+        throw new Error(`Unknown type: ${node.type}`);
     }
-    case 'nested':
-      return `${getIndent(depth)}${node.key}: ${stylish(node.children, depth + 1)}`
-    default:
-      throw new Error(`Unknown type: ${node.type}`)
-    }
-  })
-  return `{\n${lines.join('\n')}\n${'    '.repeat(depth - 1)}}`
-}
+  });
+  return `{\n${lines.join('\n')}\n${getIndent(depth)}}`;
+};
 
-export default stylish
+export default stylish;
