c1dd259de4ce2dbb59bb7e074f3e3fdb06ebe60d
Pentrick <bibenina09@mail.ru>
2025-07-08 13:26:45 +0300

    исправлен стиль eslint.config.mjs под Hexlet

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,37 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
 
-export default defineConfig([{
-    extends: compat.extends("eslint:recommended"),
+export default defineConfig([
+  {
+    extends: compat.extends('eslint:recommended'),
 
     languageOptions: {
-        globals: {
-            ...globals.node,
-            ...globals.jest,
-        },
-
-        ecmaVersion: "latest",
-        sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
 
-    rules: {},
-}]);
+    rules: {
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      indent: ['error', 2],
+      'comma-dangle': ['error', 'always-multiline'],
+      'eol-last': ['error', 'always'],
+    },
+  },
+])
