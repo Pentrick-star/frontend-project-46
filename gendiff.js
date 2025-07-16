f0ede5893cdd7910c48c7cd52d1e82b1d185a4cc
Pentrick <bibenina09@mail.ru>
2025-07-16 21:15:45 +0300

    убраны точки с запятой

--- gendiff.js
+++ gendiff.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import { Command } from 'commander'
 
-const program = new Command();
+const program = new Command()
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
 
-program.parse();
+program.parse()
