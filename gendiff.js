c65faf4a814d366864aade0f60e74d7e33d3aa94
Pentrick <bibenina09@mail.ru>
2025-07-16 21:27:19 +0300

    шаг 2: добавить аргументы filepath1, filepath2 и опцию -f для выбора формата

--- gendiff.js
+++ gendiff.js
@@ -8,5 +8,9 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action(() => {}) // Пустое действие пока, чтобы не падало
 
 program.parse()
