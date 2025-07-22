c6e8518a80b651c5d95d98afa2a3ec9396c463a7
Pentrick <bibenina09@mail.ru>
2025-07-23 01:12:10 +0300

    feat: добавлена опция --format для выбора формата вывода, обновлена справка

--- gendiff.js
+++ gendiff.js
@@ -9,9 +9,10 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
+  .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    const diff = genDiff(filepath1, filepath2)
+  .action((filepath1, filepath2, options) => {
+    const diff = genDiff(filepath1, filepath2, options.format)
     console.log(diff)
   })
 
