d431efe65164b463f43d5be88f742211626e4e1c
Pentrick <bibenina09@mail.ru>
2025-07-23 01:23:42 +0300

    feat: чтение и парсинг json-файлов, добавлены тестовые файлы и парсер

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const genDiff = () => ''
+import parseFile from './parsers.js'
+
+const genDiff = (filepath1, filepath2) => {
+  const data1 = parseFile(filepath1)
+  const data2 = parseFile(filepath2)
+  // Пока просто возвращаем оба объекта для проверки
+  return JSON.stringify({ data1, data2 }, null, 2)
+}
+
 export default genDiff 
---
+++ parsers.js
@@ -0,0 +1,14 @@
+import fs from 'fs'
+import path from 'path'
+
+const parseFile = (filepath) => {
+  const absolutePath = path.resolve(process.cwd(), filepath)
+  const data = fs.readFileSync(absolutePath, 'utf-8')
+  const ext = path.extname(filepath)
+  if (ext === '.json') {
+    return JSON.parse(data)
+  }
+  throw new Error(`Unknown file extension: ${ext}`)
+}
+
+export default parseFile 
