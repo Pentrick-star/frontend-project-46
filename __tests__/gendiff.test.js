ab2e922e46a70d3e49cbf5479a17ffb17198dcc8
Pentrick <bibenina09@mail.ru>
2025-07-24 03:13:10 +0300

    исправлены отступы

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,14 +1,17 @@
 import { test, expect } from 'vitest'
-import { fileURLToPath } from 'url'
 import path from 'path'
 import fs from 'fs'
+import { fileURLToPath } from 'url'
 import genDiff from '../code/index.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename)
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim()
+const getFixturePath = (filename) =>
+  path.join(__dirname, '..', '__fixtures__', filename)
+
+const readFile = (filename) =>
+  fs.readFileSync(getFixturePath(filename), 'utf-8').trim()
 
 test('gendiff plain json (nested)', () => {
   const file1 = getFixturePath('file1.json')
--- __tests__/gendiffPlain.test.js
+++ __tests__/gendiffPlain.test.js
@@ -7,8 +7,11 @@ import genDiff from '../code/index.js'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename)
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim()
+const getFixturePath = filename =>
+  path.join(__dirname, '..', '__fixtures__', filename)
+
+const readFile = filename =>
+  fs.readFileSync(getFixturePath(filename), 'utf-8').trim()
 
 const file1 = getFixturePath('file1.json')
 const file2 = getFixturePath('file2.json')
